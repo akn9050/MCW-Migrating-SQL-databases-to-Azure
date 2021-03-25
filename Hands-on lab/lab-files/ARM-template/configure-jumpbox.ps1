a868a8de8dce8c224532aa3d46b2384281f9c25e
Sumit Malik <45102602+sumitmalik51@users.noreply.github.com>
2021-03-26 04:52:23 +0530

    Update configure-jumpbox.ps1

--- Hands-on lab/lab-files/ARM-template/configure-jumpbox.ps1
+++ Hands-on lab/lab-files/ARM-template/configure-jumpbox.ps1
@@ -1,3 +1,50 @@
+Param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AzureUserName,
+
+    [string]
+    $AzurePassword,
+
+    [string]
+    $AzureTenantID,
+
+    [string]
+    $AzureSubscriptionID,
+
+    [string]
+    $ODLID,
+
+    [string]
+    $DeploymentID,
+
+    [string]
+    $InstallCloudLabsShadow
+
+   
+)
+
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+
+#Import Common Functions
+$path = pwd
+$path=$path.Path
+$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $commonscriptpath
+
+# Run Imported functions from cloudlabs-windows-functions.ps1
+WindowsServerCommon
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
+CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID
+InstallModernVmValidator
+choco install sql-server-management-studio
+
+
+# Disable Internet Explorer Enhanced Security Configuration
 function Disable-InternetExplorerESC {
     $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
     $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
@@ -7,14 +54,54 @@ function Disable-InternetExplorerESC {
     Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
 }
 
-# Disable IE ESC
-Disable-InternetExplorerESC
-
 # Download and extract the starter solution files
-Invoke-WebRequest 'https://github.com/microsoft/MCW-Migrating-SQL-databases-to-Azure/archive/master.zip' -OutFile 'C:\MCW.zip'
+Invoke-WebRequest 'https://experienceazure.blob.core.windows.net/templates/migrating-sql-database-to-azure(2)/MCW.zip' -OutFile 'C:\MCW.zip'
 Expand-Archive -LiteralPath 'C:\MCW.zip' -DestinationPath 'C:\hands-on-lab' -Force
 
-# Download and install SQL Server Management Studio
-Invoke-WebRequest 'https://go.microsoft.com/fwlink/?linkid=2105412' -OutFile 'C:\SSMS-Setup.exe'
-$pathArgs = {C:\SSMS-Setup.exe /Install /Quiet /Norestart /Logs log.txt}
-Invoke-Command -ScriptBlock $pathArgs
+$directoryInfo = Get-ChildItem "C:\hands-on-lab\MCW-Migrating-SQL-databases-to-Azure-master" | Measure-Object
+$dir = $directoryInfo.count
+
+If ($dir -eq 0)
+{
+Remove-Item "C:\MCW.zip"
+Invoke-WebRequest 'https://experienceazure.blob.core.windows.net/templates/migrating-sql-database-to-azure(2)/MCW.zip' -OutFile 'C:\MCW.zip'#Condition to check if the lab files are present
+
+    Expand-Archive -LiteralPath 'C:\MCW.zip' -DestinationPath 'C:\hands-on-lab' -Force
+
+}
+
+# Download and install Data Mirgation Assistant
+Invoke-WebRequest 'https://download.microsoft.com/download/C/6/3/C63D8695-CEF2-43C3-AF0A-4989507E429B/DataMigrationAssistant.msi' -OutFile 'C:\DataMigrationAssistant.msi'
+Start-Process -file 'C:\DataMigrationAssistant.msi' -arg '/qn /l*v C:\dma_install.txt' -passthru | wait-process
+Sleep 5
+
+
+
+#Download and Install edge
+
+        $WebClient = New-Object System.Net.WebClient
+
+        $WebClient.DownloadFile("http://dl.delivery.mp.microsoft.com/filestreamingservice/files/6d88cf6b-a578-468f-9ef9-2fea92f7e733/MicrosoftEdgeEnterpriseX64.msi","C:\Packages\MicrosoftEdgeBetaEnterpriseX64.msi")
+
+        sleep 5
+
+        
+
+                   Start-Process msiexec.exe -Wait '/I C:\Packages\MicrosoftEdgeBetaEnterpriseX64.msi /qn' -Verbose 
+
+        sleep 5
+
+        $WshShell = New-Object -comObject WScript.Shell
+
+        $Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\Azure Portal.lnk")
+
+        $Shortcut.TargetPath = """C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"""
+
+        $argA = """https://portal.azure.com"""
+
+        $Shortcut.Arguments = $argA 
+
+        $Shortcut.Save()
+
+
+Restart-Computer
