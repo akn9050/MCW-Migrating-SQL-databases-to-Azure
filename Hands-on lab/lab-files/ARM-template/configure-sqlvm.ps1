32f12e7ada6088bf0c70264df601c3d9c9958643
Sumit Malik <45102602+sumitmalik51@users.noreply.github.com>
2021-03-26 04:10:43 +0530

    Update configure-sqlvm.ps1

--- Hands-on lab/lab-files/ARM-template/configure-sqlvm.ps1
+++ Hands-on lab/lab-files/ARM-template/configure-sqlvm.ps1
@@ -105,3 +105,43 @@ Enable-ServiceBroker
 
 # Create the WorkshopUser user
 Config-SqlDatabaseLogin
+
+Sleep 10
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+refreshenv
+choco install dotnetfx -y -force
+
+Download the database backup file from the GitHub repo
+Invoke-WebRequest 'https://raw.githubusercontent.com/microsoft/MCW-Migrating-SQL-databases-to-Azure/master/Hands-on%20lab/lab-files/Database/WideWorldImporters.bak' -OutFile 'C:\WideWorldImporters.bak'
+Download and install Data Mirgation Assistant
+Invoke-WebRequest 'https://download.microsoft.com/download/C/6/3/C63D8695-CEF2-43C3-AF0A-4989507E429B/DataMigrationAssistant.msi' -OutFile 'C:\DataMigrationAssistant.msi'
+Start-Process -file 'C:\DataMigrationAssistant.msi' -arg '/qn /l*v C:\dma_install.txt' -passthru | wait-process
+
+
+
+#Download and Install edge
+
+    $WebClient = New-Object System.Net.WebClient
+
+    $WebClient.DownloadFile("http://dl.delivery.mp.microsoft.com/filestreamingservice/files/6d88cf6b-a578-468f-9ef9-2fea92f7e733/MicrosoftEdgeEnterpriseX64.msi","C:\Packages\MicrosoftEdgeBetaEnterpriseX64.msi")
+
+    sleep 5
+
+    
+
+               Start-Process msiexec.exe -Wait '/I C:\Packages\MicrosoftEdgeBetaEnterpriseX64.msi /qn' -Verbose 
+
+    sleep 5
+
+    $WshShell = New-Object -comObject WScript.Shell
+
+    $Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\Azure Portal.lnk")
+
+    $Shortcut.TargetPath = """C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"""
+
+    $argA = """https://portal.azure.com"""
+
+    $Shortcut.Arguments = $argA 
+
+    $Shortcut.Save()
